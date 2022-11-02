3857bcff6da9e0cc3301873d0458fc2443504c1e
Rongpu Zhou <rongpu.zhou@gmail.com>
2022-11-01 17:22:12 -0700

    .

--- misc/misc/get_sweep_columns.py
+++ misc/misc/get_sweep_columns.py
@@ -47,7 +47,7 @@ def is_in_box(objs, radecbox, ra_col='RA', dec_col='DEC'):
     return ii
 
 
-def read_sweep_columns(sweep_fn, field):
+def read_sweep_columns(sweep_fn, field, pz_dir=None):
 
     cat = Table(fitsio.read(sweep_fn, columns=['OBJID', 'BRICKID', 'RELEASE']))
     targetid = encode_targetid(cat['OBJID'], cat['BRICKID'], cat['RELEASE'])
@@ -142,9 +142,10 @@ def get_sweep_columns(cat, columns, n_processes=128, pz_dir=None):
     mask = np.array([np.any(is_in_box(cat_basic_south, sweep_radec)) for sweep_radec in sweep_radec_list_south])
     sweep_fn_list_south = np.unique(sweep_fn_list_south[mask])
 
-    global pz_dir
-    zipped_arg_list = list(zip(sweep_fn_list_north, ['north']*len(sweep_fn_list_north)))
-    zipped_arg_list += list(zip(sweep_fn_list_south, ['south']*len(sweep_fn_list_south)))
+    # zipped_arg_list = list(zip(sweep_fn_list_north, ['north']*len(sweep_fn_list_north)))
+    # zipped_arg_list += list(zip(sweep_fn_list_south, ['south']*len(sweep_fn_list_south)))
+    zipped_arg_list = list(zip(sweep_fn_list_north, ['north']*len(sweep_fn_list_north), [pz_dir]*len(sweep_fn_list_north)))
+    zipped_arg_list += list(zip(sweep_fn_list_south, ['south']*len(sweep_fn_list_south), [pz_dir]*len(sweep_fn_list_south)))
 
     with Pool(processes=n_processes) as pool:
         res = pool.starmap(read_sweep_columns, zipped_arg_list, chunksize=1)
