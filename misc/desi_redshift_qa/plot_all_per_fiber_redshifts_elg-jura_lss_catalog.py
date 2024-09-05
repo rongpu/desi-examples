cda422f3aae6845ab6dec8ec0ecd1afd905bf59a
Rongpu Zhou <rongpu.zhou@gmail.com>
2024-09-05 13:38:02 -0700

    .

---
+++ misc/desi_redshift_qa/plot_all_per_fiber_redshifts_elg-jura_lss_catalog.py
@@ -0,0 +1,178 @@
+from __future__ import division, print_function
+import sys, os, glob, time, warnings, gc
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.table import Table, vstack, hstack, join
+import fitsio
+# from astropy.io import fits
+
+from scipy.stats import kstest
+from scipy.interpolate import interp1d
+
+from datetime import datetime, timedelta
+
+params = {'legend.fontsize': 'large',
+          'axes.labelsize': 'large',
+          'axes.titlesize': 'large',
+          'xtick.labelsize': 'large',
+          'ytick.labelsize': 'large',
+          'figure.facecolor': 'w'}
+plt.rcParams.update(params)
+
+min_nobs = 50
+
+tracer = 'ELG'
+
+columns = ['COADD_FIBERSTATUS', 'COADD_NUMEXP', 'COADD_NUMNIGHT', 'DELTACHI2', 'DESI_TARGET', 'FIBER', 'LASTNIGHT', 'MASKBITS', 'SPECTYPE', 'SUBTYPE', 'DEC', 'RA', 'TARGETID', 'TILEID', 'TSNR2_BGS', 'TSNR2_LRG', 'Z_not4clus', 'ZWARN', 'OII_FLUX', 'OII_FLUX_IVAR']
+cat = Table(fitsio.read('/global/cfs/cdirs/desi/survey/catalogs/DA2/LSS/jura-v1/LSScats/test/ELG_LOPnotqso_full_noveto.dat.fits', columns=columns))
+print(len(cat))
+
+if 'Z_not4clus' in cat.colnames:
+    cat.rename_column('Z_not4clus', 'Z')
+
+# cat = Table(fitsio.read('/global/cfs/cdirs/desicollab/users/rongpu/redshift_qa/jura_data/{}.fits'.format(tracer.lower())))
+
+cat['EFFTIME_BGS'] = 0.1400 * cat['TSNR2_BGS']
+cat['EFFTIME_LRG'] = 12.15 * cat['TSNR2_LRG']
+
+# Remove FIBERSTATUS!=0 fibers
+mask = cat['COADD_FIBERSTATUS']==0
+print('FIBERSTATUS   ', np.sum(~mask), np.sum(mask), np.sum(~mask)/len(mask))
+cat = cat[mask]
+
+# Remove "no data" fibers
+mask = cat['ZWARN'] & 2**9==0
+print('No data   ', np.sum(~mask), np.sum(mask), np.sum(~mask)/len(mask))
+cat = cat[mask]
+
+# Require a minimum depth for the cat coadd
+if tracer=='BGS_ANY':
+    min_depth = 160
+    mask = cat['EFFTIME_BGS']>min_depth
+else:
+    min_depth = 800.
+    mask = cat['EFFTIME_LRG']>min_depth
+print('Min depth   ', np.sum(~mask), np.sum(mask), np.sum(~mask)/len(mask))
+cat = cat[mask]
+
+mask = (cat['OII_FLUX']>0) & (cat['OII_FLUX_IVAR']>0)
+mask &= np.log10(cat['OII_FLUX'] * np.sqrt(cat['OII_FLUX_IVAR'])) > 0.9 - 0.2 * np.log10(cat['DELTACHI2'])
+print('OII cut  ', np.sum(~mask), np.sum(mask), np.sum(~mask)/len(mask))
+cat = cat[mask]
+
+# Apply ELG mask
+main_dir = '/global/cfs/cdirs/desi/users/rongpu/targets/dr9.0/1.1.1/resolve'
+tmp = Table(fitsio.read(os.path.join(main_dir, 'dr9_elg_1.1.1_basic.fits'), columns=['TARGETID']))
+tmp2 = Table(fitsio.read(os.path.join(main_dir, 'dr9_elg_1.1.1_elgmask_v1.fits.gz')))
+elg = hstack([tmp, tmp2])
+
+print(len(elg))
+mask = elg['elg_mask']==0
+elg = elg[mask]
+print(len(elg))
+
+mask = np.in1d(cat['TARGETID'], elg['TARGETID'])
+cat = cat[mask]
+print('ELG mask   ', np.sum(~mask), np.sum(mask), np.sum(~mask)/len(mask))
+
+fiberstats = Table()
+fiberstats['FIBER'], fiberstats['n_tot'] = np.unique(cat['FIBER'], return_counts=True)
+
+print(np.median(fiberstats['n_tot']))
+
+pvalue_threshold = 1e-4
+
+# # # outliers = []
+# outliers = [53, 55, 56, 59, 64, 69, 185, 299, 333, 436, 551, 552, 553, 556, 562, 650, 675, 700, 725, 793, 817, 918, 919, 920, 997, 998, 1008, 1089, 1098, 1113, 1124, 1149, 1157, 1174, 1261, 1269, 1295, 1296, 1659, 1711, 1725, 1733, 1735, 1740, 1743, 1750, 1754, 1774, 1824, 1825, 1849, 1850, 1874, 1875, 1899, 1900, 1950, 1971, 1972, 2022, 2098, 2099, 2124, 2131, 2138, 2148, 2170, 2199, 2249, 2263, 2315, 2487, 2623, 2627, 2775, 2782, 2824, 2841, 2875, 2899, 2967, 3234, 3235, 3237, 3238, 3240, 3241, 3245, 3246, 3250, 3251, 3253, 3275, 3358, 3359, 3399, 3504, 3506, 3510, 3514, 3536, 3618, 3700, 3825, 3875, 3892, 3969, 3983, 3993, 3994, 4275, 4300, 4324, 4325, 4374, 4524, 4549, 4574, 4649, 4699, 4720, 4775, 4787, 4799, 4874, 4875, 4891, 4899, 4915]
+
+# for ii in range(2):
+
+#     print('iteration', ii)
+
+#     mask = ~np.in1d(cat['FIBER'], outliers)
+#     allz = np.sort(np.array(cat['Z'][mask]))
+#     x = allz.copy()
+#     y = np.linspace(0, 1, len(x))
+#     cdf = interp1d(x, y, fill_value=(0, 1), bounds_error=False)
+
+#     fiberstats['pvalue'] = 0.
+#     for index, fiber in enumerate(fiberstats['FIBER']):
+#         # if index%1000==0:
+#         #     print(index, len(fiberstats))
+#         mask = cat['FIBER']==fiberstats['FIBER'][index]
+#         fiberstats['pvalue'][index] = kstest(cat['Z'][mask], cdf).pvalue
+
+#     mask_outlier = fiberstats['pvalue']<pvalue_threshold
+#     mask_outlier &= fiberstats['n_tot']>=min_nobs
+#     print(np.sum(mask_outlier))
+#     outliers = np.array(np.sort(fiberstats['FIBER'][mask_outlier]))
+#     print('Outlier fibers:', list(outliers))
+#     print()
+
+fiberstats.write('/global/cfs/cdirs/desicollab/users/rongpu/redshift_qa/jura_data/ks_fiberstats_{}_lss.fits'.format(tracer.lower()), overwrite=True)
+
+fiberstats = Table(fitsio.read('/global/cfs/cdirs/desicollab/users/rongpu/redshift_qa/jura_data/ks_fiberstats_{}_lss.fits'.format(tracer.lower())))
+print(len(fiberstats))
+
+mask_outlier = fiberstats['pvalue']<pvalue_threshold
+mask_outlier &= fiberstats['n_tot']>=min_nobs
+outliers = np.array(np.sort(fiberstats['FIBER'][mask_outlier]))
+print(len(outliers))
+
+# mask_outlier = np.in1d(fiberstats['FIBER'], outliers)
+# print(np.sum(mask_outlier))
+
+# fiberstats.sort('pvalue')
+# fiberstats[mask_outlier]
+
+# fiberstats.sort('FIBER')
+# fiberstats[mask_outlier]
+
+datemin = datetime.strptime(str(cat['LASTNIGHT'].min()), '%Y%m%d') - timedelta(days=20)
+datemax = datetime.strptime(str(cat['LASTNIGHT'].max()), '%Y%m%d') + timedelta(days=20)
+
+bin_size = 0.02
+bins = np.arange(-0.01, 10, bin_size)
+bin_centers = (bins[1:]+bins[:-1])/2
+
+from matplotlib.backends.backend_pdf import PdfPages
+with PdfPages('/global/cfs/cdirs/desicollab/users/rongpu/redshift_qa/per_fiber_qa/jura/per_fiber_redshifts_{}_lss_catalog.pdf'.format(tracer.lower())) as pdf:
+    # for ii in range(1):
+    for ii in range(1000):
+        fig, axes = plt.subplots(5, 2, figsize=(20, 24), gridspec_kw={'width_ratios': [0.55, 1.5]})
+        for fiber in range(ii*5, ii*5+5):
+            if fiber%100==0:
+                print('FIBER {}'.format(fiber))
+            mask = cat['FIBER']==fiber
+            if np.sum(mask)==0:
+                ax = axes[fiber%5, 0]
+                ax.set_title('FIBER {} {}'.format(fiber, tracer))
+                ax = axes[fiber%5, 1]
+                ax.set_title('0 objects')
+                continue
+            index = np.where(fiberstats['FIBER']==fiber)[0][0]
+
+            ax = axes[fiber%5, 0]
+            vmin, vmax = cat['Z'][mask].min(), cat['Z'][mask].max()
+            vmin = vmin - (vmax-vmin)*0.05
+            vmax = vmax + (vmax-vmin)*0.05
+            ax.hist(bin_size*np.digitize(cat['Z'][mask], bins=bins), bins=bins, label='single fiber', rasterized=True)
+            mask1 = ~np.in1d(cat['FIBER'], fiberstats['FIBER'][mask_outlier])
+            ax.hist(bin_size*np.digitize(cat['Z'][mask1], bins=bins), bins=bins, histtype='step', weights=np.full(np.sum(mask1), np.sum(mask)/np.sum(mask1)), color='red', alpha=1., label='all good fibers', rasterized=True)
+            ax.set_title('FIBER {} {}  p-value = {:.5g}'.format(fiber, tracer, fiberstats['pvalue'][index]))
+            ax.set_xlabel('Redshift')
+            ax.set_xlim(vmin, vmax)
+            ax.legend(loc='upper right')
+
+            ax = axes[fiber%5, 1]
+            cat1 = cat[mask]
+            dates = np.asarray([datetime.strptime(str(night), '%Y%m%d') for night in cat1['LASTNIGHT']])
+            ax.plot(dates, cat1['Z'], '.', alpha=np.minimum(0.5, 1.*600/len(cat1)), rasterized=True)
+            ax.set_title('{} objects'.format(len(cat1)))
+            ax.set_xlabel('date')
+            ax.set_ylabel('Redshift')
+            ax.set_xlim(datemin, datemax)
+            plt.tight_layout()
+        pdf.savefig(dpi=50)
+        plt.close()
+
---
+++ misc/desi_redshift_qa/plot_redshift_vs_fiber-zcatalog-compare_with_y1.py
@@ -0,0 +1,209 @@
+# Use zcatalog instead of the LSS catalog
+# Make 2-D density plots of redshift vs fiber
+# Example:
+# python plot_redshift_vs_fiber-zcatalog-compare_with_y1.py --tracer BGS_ANY -v jura --plot_dir /global/cfs/cdirs/desi/users/rongpu/redshift_qa/z_vs_fiber/Y3_jura_zcatalog_y1_subset
+# python plot_redshift_vs_fiber-zcatalog-compare_with_y1.py --tracer LRG -v jura --plot_dir /global/cfs/cdirs/desi/users/rongpu/redshift_qa/z_vs_fiber/Y3_jura_zcatalog_y1_subset
+# python plot_redshift_vs_fiber-zcatalog-compare_with_y1.py --tracer ELG -v jura --plot_dir /global/cfs/cdirs/desi/users/rongpu/redshift_qa/z_vs_fiber/Y3_jura_zcatalog_y1_subset
+# python plot_redshift_vs_fiber-zcatalog-compare_with_y1.py --tracer QSO -v jura --plot_dir /global/cfs/cdirs/desi/users/rongpu/redshift_qa/z_vs_fiber/Y3_jura_zcatalog_y1_subset
+
+from __future__ import division, print_function
+import sys, os, glob, time, warnings, gc, argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.table import Table, vstack, hstack, join
+import fitsio
+
+params = {'figure.facecolor': 'w'}
+plt.rcParams.update(params)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--tracer", help="tracer type (BGS_ANY, LRG, ELG, or QSO)", required=True)
+parser.add_argument('-v', '--version', default='iron', required=False)
+parser.add_argument("--fn", help="path of the catalog file", default=None)
+parser.add_argument("--plot_dir", help="directory to save the plots", default=None)
+
+args = parser.parse_args()
+tracer = args.tracer.upper()
+version = args.version
+fn = args.fn
+plot_dir = args.plot_dir
+
+if not os.path.isdir(plot_dir):
+    try:
+        os.makedirs(plot_dir)
+    except:
+        pass
+
+z_bins_dict = {'BGS_ANY': np.arange(-0.025, 0.7, 0.02), 'LRG': np.arange(-0.025, 1.5, 0.025), 'ELG': np.arange(0.6, 1.8, 0.025), 'QSO': np.arange(-0.025, 4.5, 0.1)}
+vmax_dict = {'BGS_ANY': 1/3e4, 'LRG': 1/4e4, 'ELG': 1/6e4, 'QSO': 1/4e4}
+
+# The following target bits are the same in both main and SV3
+target_bits = {'LRG': 0, 'ELG': 1, 'QSO': 2, 'BGS_ANY': 60}
+target_bit = target_bits[tracer]
+
+if fn is None:
+    fn_dict = {'BGS_ANY': 'ztile-main-bright-cumulative.fits', 'LRG': 'ztile-main-dark-cumulative.fits', 'ELG': 'ztile-main-dark-cumulative.fits', 'QSO': 'ztile-main-dark-cumulative.fits'}
+    # fn = os.path.join('/global/cfs/cdirs/desi/spectro/redux/iron/zcatalog', fn_dict[tracer])
+    fn = os.path.join('/global/cfs/cdirs/desi/spectro/redux/{}/zcatalog/v1'.format(version), fn_dict[tracer])
+fn_y1 = fn.replace('jura', 'iron').replace('v1', 'v0')
+print(fn_y1)
+
+min_nobs = 100
+
+########################################################################################################################
+
+cat = Table(fitsio.read(fn_y1, columns=['DESI_TARGET']))
+idx = np.where(cat['DESI_TARGET'] & 2**target_bit > 0)[0]
+cat = Table(fitsio.read(fn_y1, rows=idx))
+print(len(cat))
+
+if 'Z_not4clus' in cat.colnames:
+    cat.rename_column('Z_not4clus', 'Z')
+
+cat['EFFTIME_BGS'] = 0.1400 * cat['TSNR2_BGS']
+cat['EFFTIME_LRG'] = 12.15 * cat['TSNR2_LRG']
+
+# Remove FIBERSTATUS!=0 fibers
+mask = cat['COADD_FIBERSTATUS']==0
+print('FIBERSTATUS   ', np.sum(~mask), np.sum(mask), np.sum(~mask)/len(mask))
+cat = cat[mask]
+
+# Remove "no data" fibers
+mask = cat['ZWARN'] & 2**9==0
+print('No data   ', np.sum(~mask), np.sum(mask), np.sum(~mask)/len(mask))
+cat = cat[mask]
+
+# Require a minimum depth for the cat coadd
+if tracer=='BGS_ANY':
+    min_depth = 160
+    mask = cat['EFFTIME_BGS']>min_depth
+else:
+    min_depth = 800.
+    mask = cat['EFFTIME_LRG']>min_depth
+print('Min depth   ', np.sum(~mask), np.sum(mask), np.sum(~mask)/len(mask))
+cat = cat[mask]
+
+if tracer=='LRG':
+    # Apply maskbits
+    maskbits = [1, 8, 9, 11, 12, 13]
+    mask = np.ones(len(cat), dtype=bool)
+    for bit in maskbits:
+        mask &= (cat['MASKBITS'] & 2**bit)==0
+    print('MASKBITS  ', np.sum(~mask), np.sum(mask), np.sum(~mask)/len(mask))
+    cat = cat[mask]
+elif tracer=='ELG':
+    # Apply maskbits
+    maskbits = [1, 11, 12, 13]
+    mask = np.ones(len(cat), dtype=bool)
+    for bit in maskbits:
+        mask &= (cat['MASKBITS'] & 2**bit)==0
+    print('MASKBITS  ', np.sum(~mask), np.sum(mask), np.sum(~mask)/len(mask))
+    cat = cat[mask]
+
+# # Remove duplicated objects
+# print(len(cat), len(np.unique(cat['TARGETID'])))
+# cat.sort('EFFTIME_LRG', reverse=True)
+# _, idx_keep = np.unique(cat['TARGETID'], return_index=True)
+# cat = cat[idx_keep]
+# print(len(cat), len(np.unique(cat['TARGETID'])))
+
+cat_y1 = cat.copy()
+
+########################################################################################################################
+
+cat = Table(fitsio.read(fn, columns=['DESI_TARGET']))
+idx = np.where(cat['DESI_TARGET'] & 2**target_bit > 0)[0]
+cat = Table(fitsio.read(fn, rows=idx))
+print(len(cat))
+
+if 'Z_not4clus' in cat.colnames:
+    cat.rename_column('Z_not4clus', 'Z')
+
+cat['EFFTIME_BGS'] = 0.1400 * cat['TSNR2_BGS']
+cat['EFFTIME_LRG'] = 12.15 * cat['TSNR2_LRG']
+
+# Remove FIBERSTATUS!=0 fibers
+mask = cat['COADD_FIBERSTATUS']==0
+print('FIBERSTATUS   ', np.sum(~mask), np.sum(mask), np.sum(~mask)/len(mask))
+cat = cat[mask]
+
+# Remove "no data" fibers
+mask = cat['ZWARN'] & 2**9==0
+print('No data   ', np.sum(~mask), np.sum(mask), np.sum(~mask)/len(mask))
+cat = cat[mask]
+
+# Require a minimum depth for the cat coadd
+if tracer=='BGS_ANY':
+    min_depth = 160
+    mask = cat['EFFTIME_BGS']>min_depth
+else:
+    min_depth = 800.
+    mask = cat['EFFTIME_LRG']>min_depth
+print('Min depth   ', np.sum(~mask), np.sum(mask), np.sum(~mask)/len(mask))
+cat = cat[mask]
+
+if tracer=='LRG':
+    # Apply maskbits
+    maskbits = [1, 8, 9, 11, 12, 13]
+    mask = np.ones(len(cat), dtype=bool)
+    for bit in maskbits:
+        mask &= (cat['MASKBITS'] & 2**bit)==0
+    print('MASKBITS  ', np.sum(~mask), np.sum(mask), np.sum(~mask)/len(mask))
+    cat = cat[mask]
+elif tracer=='ELG':
+    # Apply maskbits
+    maskbits = [1, 11, 12, 13]
+    mask = np.ones(len(cat), dtype=bool)
+    for bit in maskbits:
+        mask &= (cat['MASKBITS'] & 2**bit)==0
+    print('MASKBITS  ', np.sum(~mask), np.sum(mask), np.sum(~mask)/len(mask))
+    cat = cat[mask]
+
+# # Remove duplicated objects
+# print(len(cat), len(np.unique(cat['TARGETID'])))
+# cat.sort('EFFTIME_LRG', reverse=True)
+# _, idx_keep = np.unique(cat['TARGETID'], return_index=True)
+# cat = cat[idx_keep]
+# print(len(cat), len(np.unique(cat['TARGETID'])))
+
+########################################################################################################################
+mask = np.in1d(cat['TARGETID'], cat_y1['TARGETID'])
+mask &= np.in1d(cat['TILEID'], cat_y1['TILEID'])
+cat = cat[mask]
+print(np.sum(mask)/len(mask), len(cat), len(cat_y1))
+########################################################################################################################
+
+fiberstats = Table()
+fiberstats['FIBER'], fiberstats['n_tot'] = np.unique(cat['FIBER'], return_counts=True)
+fiberstats['weight'] = 1/fiberstats['n_tot']*np.median(fiberstats['n_tot'])
+cat = join(cat, fiberstats[['FIBER', 'weight']], join_type='outer')
+too_few_fibers = fiberstats['FIBER'][fiberstats['n_tot']<min_nobs]
+
+fig, ax = plt.subplots(10, 1, figsize=(16, 20))
+for index in range(10):
+    fiber_min, fiber_max = index*500-0.5, (index+1)*500-0.5
+    mask = (cat['FIBER']>fiber_min) & (cat['FIBER']<fiber_max)
+    xbins, ybins = np.linspace(fiber_min, fiber_max, 501), z_bins_dict[tracer]
+    ybins = ybins - np.diff(ybins)[0]/2  # center one of the bins at z=0
+    ax[index].hist2d(cat['FIBER'][mask], cat['Z'][mask], bins=[xbins, ybins], vmin=0, vmax=len(cat)*vmax_dict[tracer])
+    ax[index].set_xlim(fiber_min, fiber_max)
+plt.tight_layout()
+if plot_dir is not None:
+    plt.savefig(os.path.join(plot_dir, 'redshift_vs_fiber_{}.png'.format(tracer.lower())))
+plt.show()
+
+# Normalize by the number of objects in each fiber
+fig, ax = plt.subplots(10, 1, figsize=(16, 20))
+for index in range(10):
+    fiber_min, fiber_max = index*500-0.5, (index+1)*500-0.5
+    mask = (cat['FIBER']>fiber_min) & (cat['FIBER']<fiber_max)
+    mask &= ~np.in1d(cat['FIBER'], too_few_fibers)  # Do not plot fibers with too few objects
+    xbins, ybins = np.linspace(fiber_min, fiber_max, 501), z_bins_dict[tracer]
+    ybins = ybins - np.diff(ybins)[0]/2  # center one of the bins at z=0
+    ax[index].hist2d(cat['FIBER'][mask], cat['Z'][mask], weights=cat['weight'][mask], bins=[xbins, ybins], vmin=0, vmax=len(cat)*vmax_dict[tracer])
+    ax[index].set_xlim(fiber_min, fiber_max)
+plt.tight_layout()
+if plot_dir is not None:
+    plt.savefig(os.path.join(plot_dir, 'redshift_vs_fiber_{}_norm.png'.format(tracer.lower())))
+plt.show()
+
